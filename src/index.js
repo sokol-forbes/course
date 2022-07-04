d9f64f93ebf1a1095666a41034545b46eaca6621
sokol-forbes <sasha.sokolovsky01@gmail.com>
2022-07-04 13:41:45 +0300

    precompilers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Post from './Post'
 import './styles/style.css'
+import './styles/less.less'
+import './styles/scss.scss'
 import WebpackLogo from './assets/webpack-logo.png'
 const post = new Post('Webpack Post Title',WebpackLogo)
 console.log('Post to string ', post.toString())
